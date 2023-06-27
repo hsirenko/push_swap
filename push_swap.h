4e29a753616a9b31a78bc1f8968cf7ca091c634e
hsirenko <sirenko.helen@gmail.com>
2023-06-27 12:50:55 +0200

    added some files

---
+++ push_swap.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: helensirenko <helensirenko@student.42.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/26 11:06:22 by helensirenko      #+#    #+#             */
+/*   Updated: 2023/06/26 11:07:28 by helensirenko     ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif
